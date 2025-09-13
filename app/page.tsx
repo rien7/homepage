6c92f53a45468602031f6008693bde16c03819a6
RieN 7z <zrien7@gmail.com>
2025-09-13 19:34:35 +0800

    fix: remove background

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,23 @@ import { HomeMain } from "./_components/home_page/Main";
 export const revalidate = 60;
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen min-w-screen gap-4">
-      <HomeMain />
-      <motion.div
-        className="[--x:0] md:[--x:200px] [--y:20px] md:[--y:0]"
-        initial={{
-          opacity: 0,
-          x: "var(--x)",
-          y: "var(--y)",
-          filter: "blur(4px)",
-        }}
-        animate={{ opacity: 1, x: 0, y: 0, filter: "blur(0px)" }}
-        transition={{ delay: 1.2, duration: 0.3 }}
-      >
-        <BlogList />
-      </motion.div>
+    <div className="flex items-center justify-center min-h-screen min-w-screen">
+      <div className="flex flex-col md:flex-row items-start justify-center gap-4">
+        <HomeMain />
+        <motion.div
+          className="[--x:0] md:[--x:200px] [--y:20px] md:[--y:0]"
+          initial={{
+            opacity: 0,
+            x: "var(--x)",
+            y: "var(--y)",
+            filter: "blur(4px)",
+          }}
+          animate={{ opacity: 1, x: 0, y: 0, filter: "blur(0px)" }}
+          transition={{ delay: 1.2, duration: 0.3 }}
+        >
+          <BlogList />
+        </motion.div>
+      </div>
     </div>
   );
 }
